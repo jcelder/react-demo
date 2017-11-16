6dd57057fc40938b17f7027e8fe2815b238b285b
Joshua Elder <aresx130@gmail.com>
2017-11-16 15:56:07 -0800

    Added stub for add button.

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,4 +1,10 @@
 import React from 'react'
 import Notes from './Notes'
 
-export default () => <Notes />
+// export default () => <Notes />
+export default () => (
+  <div>
+    <button onClick={() => console.log('Add note')}>+</button>
+    <Notes />
+  </div>
+)
